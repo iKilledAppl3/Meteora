cd998928d89ee2c60343be5b2f43e898c690600a
J.K. Hayslip <ipodnewbee234@icloud.com>
2017-08-13 13:14:39 -0400

    fix for image

--- Preferences/Headers.h
+++ Preferences/Headers.h
@@ -1,7 +1,7 @@
 #import <UIKit/UIKit.h>
 #import <Preferences/PSListController.h>
 
-static NSString *meteoraImagePath = @"/Library/Application Support/Meteora/Durango.png";
+static NSString *meteoraImagePath = @"/Library/Application Support/Meteora/Meteora.png";
 
 @interface HBListController : PSListController
 @end 
